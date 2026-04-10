b3f5929b54135365c69b0868f33197f760a05fc3
Darrell Ng <darrell.ng.wn@gmail.com>
2026-04-10 10:33:23 +0800

    Initial Commit

---
+++ BeeAyy/BeeAyyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeeAyyApp.swift
+//  BeeAyy
+//
+//  Created by Darrell Ng on 10/4/26.
+//
+
+import SwiftUI
+
+@main
+struct BeeAyyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
